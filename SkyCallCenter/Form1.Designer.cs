7b2fb1d9f3f214e6e89a1efd4645cfd15d732b4b
Pablo <pabllobeg@hotmail.com>
2017-06-30 08:44:16 -0300

    First Commit

---
+++ SkyCallCenter/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SkyCallCenter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.RecordButton = new System.Windows.Forms.Button();
+            this.statusTextBox = new System.Windows.Forms.TextBox();
+            this.StatusOfRecording = new System.Windows.Forms.Label();
+            this.NotifycationIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // RecordButton
+            // 
+            this.RecordButton.Font = new System.Drawing.Font("Trebuchet MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RecordButton.Location = new System.Drawing.Point(120, 12);
+            this.RecordButton.Name = "RecordButton";
+            this.RecordButton.Size = new System.Drawing.Size(163, 49);
+            this.RecordButton.TabIndex = 0;
+            this.RecordButton.Text = "Gravar";
+            this.RecordButton.UseVisualStyleBackColor = true;
+            this.RecordButton.Click += new System.EventHandler(this.Record_Click);
+            // 
+            // statusTextBox
+            // 
+            this.statusTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusTextBox.Location = new System.Drawing.Point(181, 87);
+            this.statusTextBox.Name = "statusTextBox";
+            this.statusTextBox.Size = new System.Drawing.Size(154, 23);
+            this.statusTextBox.TabIndex = 1;
+            this.statusTextBox.Text = "Não está gravando.";
+            this.statusTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // StatusOfRecording
+            // 
+            this.StatusOfRecording.AutoSize = true;
+            this.StatusOfRecording.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StatusOfRecording.Location = new System.Drawing.Point(24, 90);
+            this.StatusOfRecording.Name = "StatusOfRecording";
+            this.StatusOfRecording.Size = new System.Drawing.Size(142, 17);
+            this.StatusOfRecording.TabIndex = 2;
+            this.StatusOfRecording.Text = "Status da Gravação: ";
+            // 
+            // NotifycationIcon
+            // 
+            this.NotifycationIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifycationIcon.Icon")));
+            this.NotifycationIcon.Text = "NotifycationIcon";
+            this.NotifycationIcon.Visible = true;
+            this.NotifycationIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotificationIcon_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 141);
+            this.Controls.Add(this.StatusOfRecording);
+            this.Controls.Add(this.statusTextBox);
+            this.Controls.Add(this.RecordButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Bat Recording";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.SkyCallCenter_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RecordButton;
+        private System.Windows.Forms.TextBox statusTextBox;
+        private System.Windows.Forms.Label StatusOfRecording;
+        private System.Windows.Forms.NotifyIcon NotifycationIcon;
+    }
+}
+
---
+++ SkyCallCenter/Form1.cs
@@ -0,0 +1,131 @@
+﻿using CSCore;
+using CSCore.Codecs.WAV;
+using CSCore.CoreAudioAPI;
+using CSCore.SoundIn;
+using CSCore.Streams;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkyCallCenter
+{
+    public partial class Form1 : Form
+    {
+        private Boolean recording = false;
+        private WasapiCapture soundIn;
+        private IWriteable writer;
+        private IWaveSource waveSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Record_Click(object sender, EventArgs events)
+        {
+           
+            if (!this.recording)
+            {
+
+                SaveFileDialog sfd = new SaveFileDialog
+                {
+                    Filter = "WAV (*.wav)|*.wav",
+                    Title = "Save",
+                    FileName = String.Empty
+                };
+                if (sfd.ShowDialog(this) == DialogResult.OK)
+                {
+                    StartCapture(sfd.FileName);
+                    this.statusTextBox.Text = "Gravando.";
+                    this.RecordButton.Text = "Parar";
+                    this.recording = true;
+                }
+
+            }
+            else
+            {
+                StopCapture();
+                this.statusTextBox.Text = "Não está gravando.";
+                this.RecordButton.Text = "Gravar";
+                this.recording = false;
+            }
+
+
+        }
+
+        private void StartCapture(string fileName)
+        {
+            var deviceEnumerator = new MMDeviceEnumerator();
+            var defaultDevice = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
+            this.soundIn = new WasapiCapture();
+            this.soundIn.Device = defaultDevice;
+
+            this.soundIn.Initialize();
+            var soundInSource = new SoundInSource(this.soundIn) { FillWithZeros = false };
+            //var singleBlockNotificationStream = new SingleBlockNotificationStream(soundInSource.ToSampleSource());
+            //this.waveSource = singleBlockNotificationStream.ToWaveSource();
+            this.waveSource = soundInSource.ToSampleSource().ToWaveSource();
+            this.writer = new WaveWriter(fileName, this.waveSource.WaveFormat);
+
+            byte[] buffer = new byte[this.waveSource.WaveFormat.BytesPerSecond / 2];
+            soundInSource.DataAvailable += (s, e) =>
+            {
+                int read;
+                while ((read = this.waveSource.Read(buffer, 0, buffer.Length)) > 0)
+                    this.writer.Write(buffer, 0, read);
+            };
+
+            soundIn.Start();
+
+
+        }
+
+        private void StopCapture()
+        {
+            if (soundIn != null)
+            {
+                this.soundIn.Stop();
+                this.soundIn.Dispose();
+                this.soundIn = null;
+                this.waveSource.Dispose();
+
+                if (writer is IDisposable)
+                    ((IDisposable)writer).Dispose();
+            }
+        }
+
+        private void SkyCallCenter_Resize(object sender, EventArgs e)
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                this.NotifycationIcon.Visible = true;
+                this.NotifycationIcon.BalloonTipTitle = "BatMorcego Avisa";
+                this.NotifycationIcon.BalloonTipText = "Continuo funcionando \n Para Gravar";
+                this.NotifycationIcon.BalloonTipIcon = ToolTipIcon.Info;
+                this.NotifycationIcon.ShowBalloonTip(1000);
+                this.Hide();
+            }else if(this.WindowState == FormWindowState.Normal)
+            {
+                this.NotifycationIcon.Visible = false;
+            }
+        }
+
+        private void NotificationIcon_DoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+    }
+}
