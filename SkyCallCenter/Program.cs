a2c78a06475f7c0cbdfa189fc533cf2a4c7408bc
Pablo <pabllobeg@hotmail.com>
2017-07-02 14:20:04 -0300

    Saving to mp3 in the right way

--- SkyCallCenter/Program.cs
+++ SkyCallCenter/Program.cs
@@ -17,7 +17,7 @@ namespace BatRecording
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new SaveAudioDialog());
         }
     }
 }
