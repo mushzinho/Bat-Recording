8a4957f32d9628a68f66418b813c122cf5606667
Pablo <pabllobeg@hotmail.com>
2017-07-12 19:33:42 -0300

    Login form created

---
+++ BatRecording/LoginForm.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace BatRecording
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.LabelLogin = new System.Windows.Forms.Label();
+            this.LabelSenha = new System.Windows.Forms.Label();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxSenha = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LabelLogin
+            // 
+            this.LabelLogin.AutoSize = true;
+            this.LabelLogin.Location = new System.Drawing.Point(56, 49);
+            this.LabelLogin.Name = "LabelLogin";
+            this.LabelLogin.Size = new System.Drawing.Size(33, 13);
+            this.LabelLogin.TabIndex = 0;
+            this.LabelLogin.Text = "Login";
+            // 
+            // LabelSenha
+            // 
+            this.LabelSenha.AutoSize = true;
+            this.LabelSenha.Location = new System.Drawing.Point(51, 90);
+            this.LabelSenha.Name = "LabelSenha";
+            this.LabelSenha.Size = new System.Drawing.Size(38, 13);
+            this.LabelSenha.TabIndex = 1;
+            this.LabelSenha.Text = "Senha";
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.Location = new System.Drawing.Point(95, 46);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(202, 20);
+            this.textBoxLogin.TabIndex = 2;
+            // 
+            // textBoxSenha
+            // 
+            this.textBoxSenha.Location = new System.Drawing.Point(95, 87);
+            this.textBoxSenha.Name = "textBoxSenha";
+            this.textBoxSenha.PasswordChar = '*';
+            this.textBoxSenha.Size = new System.Drawing.Size(202, 20);
+            this.textBoxSenha.TabIndex = 3;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(138, 129);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(103, 36);
+            this.buttonLogin.TabIndex = 4;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 196);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textBoxSenha);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.LabelSenha);
+            this.Controls.Add(this.LabelLogin);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fazer Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LabelLogin;
+        private System.Windows.Forms.Label LabelSenha;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxSenha;
+        private System.Windows.Forms.Button buttonLogin;
+    }
+}
---
+++ BatRecording/LoginForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatRecording
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (textBoxLogin.Text.Length > 2 && textBoxSenha.Text.Length > 2)
+            {
+                bool result = new Employer().Autentication(textBoxLogin.Text, textBoxSenha.Text);
+                if (result)
+                {
+                    MessageBox.Show(@"Logado");
+                }
+                else
+                {
+                    MessageBox.Show(@"Login ou senha inválidos.");
+                }
+            }
+            else
+            {
+                MessageBox.Show(@"Login ou senha devem ter mais de 2 dígitos.");
+            }
+            
+        }
+    }
+}
+
