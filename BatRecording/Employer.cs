4bd8771de410ec5a58971d2bbfbe9696bd16cd32
Pablo <pabllobeg@hotmail.com>
2017-07-11 22:05:52 -0300

    Added ManageFTP class, Employer class to handle adding and deleting Employers

--- BatRecording/Employer.cs
+++ BatRecording/Employer.cs
@@ -1,22 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using FluentFTP;
 
 namespace BatRecording
 {
     class Employer
     {
-        private string name;
-        private string login;
-        private string pass;
+        private string _name;
+        private string _login;
+        private string _pass;
+        private List<string> _employers = new List<string>();
+        private const string BaseUrlEmployersFile = "/employers.txt";
 
-        public Employer(string name, string login, string pass)
+        private void GetAllEmployers()
         {
-            this.name = name;
-            this.login = login;
-            this.pass = pass;
+            var manageFtp = new ManageFtp("pablo", "pablo");
+            var client = manageFtp.Client;
+            Stream myFileStream = client.OpenRead(BaseUrlEmployersFile, FtpDataType.Binary);
+            StreamReader reader = new StreamReader(myFileStream, Encoding.UTF8);
+
+            string[] allEmployers = reader.ReadToEnd().Split('\n');
+            foreach (var employer in allEmployers)
+            {
+                _employers.Add(employer);
+            }
+            
+            reader.Close();
+            client.Disconnect();
+        }
+
+        private void SaveEmployerToFile(string employer)
+        {
+            var manageFtp = new ManageFtp("pablo", "pablo");
+            var client = manageFtp.Client;
+            Stream myFileStream = client.OpenAppend(BaseUrlEmployersFile, FtpDataType.Binary);
+          
+            StreamWriter writer = new StreamWriter(myFileStream);
+           
+            writer.WriteLine(employer);
+            writer.Flush();
+            writer.Close();
+            client.GetReply();
+            client.Disconnect();
+            
+        }
+        public bool CreateEmployer(string name, string login, string pass)
+        {
+            this.GetAllEmployers();
+            if (_employers.Any(lineEmployer => lineEmployer.Contains(login)))
+            {
+                MessageBox.Show(@"Já existe um funcionario com esse login.");
+                return false;
+            }
+            //TODO: NAO TA CRIANDO COM MESMO NOME
+           
+            this._name = name;
+            this._login = login;
+            this._pass = pass;
+
+            string employer = _name + ":" + _login + ":" + _pass;
+            this.SaveEmployerToFile(employer);
+
+            return true;
         }
 
         public bool DeleteEmployer(string login)
@@ -30,6 +80,7 @@ namespace BatRecording
 
             return true;
         }
+        
 
   
     }
---
+++ BatRecording/ManageFTP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using FluentFTP;
+
+namespace BatRecording
+{
+    class ManageFtp
+
+    {
+        public FtpClient Client { get; }
+
+        public ManageFtp(string username, string pass, string server = "127.0.0.1")
+        {
+             this.Client = new FtpClient(server)    
+            {
+                Port = 29,
+                Credentials = new NetworkCredential(username, pass)
+            };
+            this.Client.Connect();
+        }
+        
+    }
+}
